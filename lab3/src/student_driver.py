5c285b4f9a19b1a25e199de675209474dc7128f0
khussei <64563578+khussei@users.noreply.github.com>
2024-12-09 08:57:38 +0000

    added my go around code

--- lab3/src/student_driver.py
+++ lab3/src/student_driver.py
@@ -3,10 +3,11 @@
 
 import sys
 import rospy
+import numpy as np
 
 from new_driver import Driver
 
-from math import atan2, sqrt
+from math import atan2, sqrt, tanh, sin
 
 
 class StudentDriver(Driver):
@@ -18,6 +19,8 @@ class StudentDriver(Driver):
 		super().__init__('odom')
 		# Set the threshold to a reasonable number
 		self._threshold = threshold
+		self.count_veer = 0 #tracks veering actions to avoid rapid direction changes
+
 
 	def close_enough_to_waypoint(self, distance, target, lidar):
 		'''
@@ -29,38 +32,73 @@ class StudentDriver(Driver):
 		if distance < self._threshold:
 			return True
 		return False
-
 	def get_twist(self, target, lidar):
-		'''
-		This function is called whenever there a current target is set and there is a lidar data
-		available.  This is where you should put your code for moving the robot.  The target point
-		is in the robot's coordinate frame.  The x-axis is positive-forwards, and the y-axis is
-		positive to the left.
-
-		The example sets constant velocities, which is clearly the wrong thing to do.  Replace this
-		code with something that moves the robot more intelligently.
-
-		Parameters:
-			target:		The current target point, in the coordinate frame of the robot (base_link) as
-						an (x, y) tuple.
-			lidar:		A LaserScan containing the new lidar data.
-
-		Returns:
-			A Twist message, containing the commanded robot velocities.
-		'''
-		angle = atan2(target[1], target[0])
-		distance = sqrt(target[0] ** 2 + target[1] **2)
-		rospy.loginfo(f'Distance: {distance:.2f}, angle: {angle:.2f}')
-
-		# This builds a Twist message with all elements set to zero.
+		# Bot Parameters:
+		#	lidar_max: lidar captures this distance around bot [m]
+		#	v_max: maximum forward speed [m/s]
+		#	t_to_closest: time to slow down in -- responsiveness [s]
+		#	r_closest: maximum distance allowed to an opject [m]
+		params = {'bot_width':0.38, 'lidar_max': 8.0, 'v_max': 0.30, 't_to_closest': 3, 'r_closest': 1.19}
+		
 		command = Driver.zero_twist()
 
-		# Forwards velocity goes here, in meters per second.
-		command.linear.x = 0.1
+		# prepping the lidar's thetas for later use
+		angle_min, angle_max, num_readings = lidar.angle_min, lidar.angle_max, len(lidar.ranges)
+		thetas = np.linspace(angle_min, angle_max, num_readings) # the lidar's thetas (consistent)
+		front_mindex, front_maxdex = round(num_readings / 4), round(3 * num_readings / 4)
+		# prepping given target
+		target_x, target_y = target
+		theta_g = atan2(target_y, target_x)
 
-		# Rotational velocity goes here, in radians per second.  Positive is counter-clockwise.
-		command.angular.z = 0.1
+		#robot movement calcs from parameters
+		d_slow_down = params['v_max'] * params['t_to_closest'] # distance bot will slow to a stop
+		turn_radius =  params['bot_width'] * 1.5
+		omega_max = params['v_max'] / turn_radius
 
+		# figuring out shortest distance to an obstruction within the relevant front of lidar scope
+		relevant_rs, relevant_thetas = [],[]
+		for i, r in enumerate(lidar.ranges):
+			y_dist = r * sin(thetas[i])
+			if abs(y_dist) <= params['r_closest']:
+				if front_mindex <= i < front_maxdex:
+				#obstruction found in front of bot
+					relevant_rs.append(r)
+					relevant_thetas.append(thetas[i])
+		unbounded_shortest = min(lidar.ranges, default=params['lidar_max'])
+		shortest = min(relevant_rs, default=params['lidar_max'])
+		bot_theta_obj = relevant_thetas[relevant_rs.index(shortest)] if relevant_rs else thetas[-1]
+		
+		# default commands and distance to target
+		command.linear.x = params['v_max']
+		command.angular.z = 0
+		distance = sqrt(target_x ** 2 + target_y ** 2)
+		
+		# obstacle avoidance logic
+		reset_veer = False
+		if shortest < d_slow_down:
+        	# obstacle in front
+			print("obstruction directly in front")
+			reset_veer = True
+			command.linear.x = params['v_max'] * tanh(shortest / d_slow_down)
+			command.angular.z = tanh(-bot_theta_obj / turn_radius)
+		elif unbounded_shortest < turn_radius and distance > unbounded_shortest:
+        	# obstacle on side
+			print("Too close to an obstacle on the side!")
+			reset_veer = True
+			command.angular.z = tanh(-bot_theta_obj / turn_radius)
+		else:
+        	# no obstacles
+			if self.count_veer > 0:
+				self.count_veer -= 1
+			else:
+				print("moving straight towards the goal!")
+				command.linear.x = params['v_max'] * tanh(distance / d_slow_down)
+				command.angular.z = tanh(theta_g / turn_radius)  # Turn toward the goal
+
+    	# only reset veer if needed
+		if reset_veer and self.count_veer == 0:
+			self.count_veer = 3
+		
 		return command
 
 
@@ -70,3 +108,4 @@ if __name__ == '__main__':
 	driver = StudentDriver()
 
 	rospy.spin()
+	
